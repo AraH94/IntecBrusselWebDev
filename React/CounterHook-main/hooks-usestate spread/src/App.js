74e06a4e26b73a7cf18b74a713059aa0428de092
AraH94 <arahoedemakers@hotmail.com>
2023-04-25 15:06:30 +0200

    first

---
+++ React/CounterHook-main/hooks-usestate spread/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+	const [state, setState] = useState({ count: 8, name: "Ara" });
+	const count = state.count;
+	const name = state.name;
+
+	// type a name in the inputfield and it will change in the text
+	const [firstName, setName] = useState('');
+	const handleChange = (e) => {
+		setName(e.target.value)
+	}
+
+	//spread operator is used to prevent overwriting previous info
+	function decrement() {
+		setState(prevState => { return { ...prevState, count: prevState.count - 1 } });
+	}
+
+	function increment() {
+		setState(prevState => { return { ...prevState, count: prevState.count + 1 } });
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Hello {firstName}</p>
+				<input type="text" value={firstName} onChange={handleChange}></input>
+				<button onClick={decrement}>-</button>
+				<span>{count}</span>
+				<button onClick={increment}>+</button>
+				<p>Name: {name}</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ React/CounterHook-main/hooks-usestate/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Count from './Count';
+import './App.css';
+
+function App() {
+  return (
+	<Count/>
+  )
+}
+
+export default App;
